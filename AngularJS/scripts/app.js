4f5a44981c95384a53dff0c5db35e8030378e55d
respag <respagqhotmail.com>
2014-01-28 17:31:31 -0500

    28 Enero 5 y 30 PM

--- AngularJS/scripts/app.js
+++ AngularJS/scripts/app.js
@@ -1,6 +1,36 @@
 ﻿"use strict";
-var app = angular.module("myApp", []);
+var app = angular.module("myApp", ['ngRoute']);
 
-app.controller("ctrl", function () {
+app.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "views/uno.html",
+        controller:"ctrl"
+    })
+    .when("/dos", {
+            templateUrl: "views/dos.html",
+            controller: "dosCtrl"
+        })
+    .otherwise({ redirectTo: "/" });
+});
 
+app.controller("ctrl", function ($scope) {
+    $scope.misDatos = { nombre: "Rubén", apellido: "Spagnuolo", alias: "respag" };
+});
+
+app.controller("dosCtrl", function ($scope) {
+
+});
+
+$(document).foundation();
+
+$(document).ready(function () {
+    $('#anchoPag').html($(window).innerWidth());
+    $(window).resize(function () {
+        $('#anchoPag').html($(window).innerWidth());
+    })
+
+    $('#altPag').html($(window).innerHeight());
+    $(window).resize(function () {
+        $('#altPag').html($(window).innerHeight());
+    })
 });
