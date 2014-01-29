7ca05d5ea6bb527252417c663e22f0187485b55f
Rubén Spagnuolo <respag@hotmail.com>
2014-01-29 02:35:24 -0500

    29 DE ENERO 2:30

---
+++ AngularJS/scripts/apps2.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+var app = angular.module("myApp",[]);
+
+app.controller("ctrl", function ($scope) {
+   // $scope.persona = { nombre: "Pepe" };
+});
+
+$(document).foundation();
+$(document).ready(function () {
+    $("#inicio").click(function () {
+        $(this).toggleClass( "active");
+    });
+});
